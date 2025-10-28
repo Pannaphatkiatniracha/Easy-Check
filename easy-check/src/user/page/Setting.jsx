1ac8359ee1fe72ad078bbc6a4ff84d8a0122269a
thitichat-11 <thitichatphoto@gmail.com>
2025-10-28 15:02:39 +0700

    ขึ้นโครงตกแต่งหน้า Profile + เชื่อมกับ mock API ให้แก้ไขข้อมูลได้แล้ว

--- easy-check/src/user/page/Setting.jsx
+++ easy-check/src/user/page/Setting.jsx
@@ -13,7 +13,11 @@ const Setting = () => {
 
             {/* โนติ */}
             <div className="d-flex justify-content-center mt-5">
-                <div className="w-75 p-2 rounded bg-warning d-flex align-items-center fw-semibold hover:scale-105 transition-all duration-200 ease-in-out">
+                  {/* hover:scale-105 = hover แล้วขยายฉ่ำๆ 105%
+                    duration-200 = ความเร็ว 0.2 s 
+                    ease-in-out ก็ให้ตอนเปลี่ยนมันสมูท*/}
+                <div className="w-75 p-2 rounded bg-warning d-flex align-items-center fw-semibold
+                hover:scale-105 transition-all duration-200 ease-in-out">
                     <i className="bi bi-bell-fill fs-4"></i> &nbsp; Notification
 
 
@@ -27,7 +31,8 @@ const Setting = () => {
 
             {/* dark/light mode */}
             <div className="d-flex justify-content-center mt-3">
-                <div className="w-75 p-2 rounded bg-warning d-flex align-items-center fw-semibold hover:scale-105 transition-all duration-200 ease-in-out">
+                <div className="w-75 p-2 rounded bg-warning d-flex align-items-center fw-semibold
+                hover:scale-105 transition-all duration-200 ease-in-out">
                     <i className="bi bi-circle-half"></i> &nbsp; Dark Mode
 
                         {/* ms คือ margin start พอเป็น ms-auto ก็คือเป็น LTR จาก L ดัน R */}
@@ -41,7 +46,8 @@ const Setting = () => {
             {/* edit profile */}
             <Link to="/profile" className='text-decoration-none d-block'>            
                 <div className="d-flex justify-content-center mt-3">
-                    <div className="w-75 p-2 rounded bg-warning d-flex align-items-center fw-semibold text-dark hover:scale-105 transition-all duration-200 ease-in-out">
+                    <div className="w-75 p-2 rounded bg-warning d-flex align-items-center fw-semibold text-dark
+                    hover:scale-105 transition-all duration-200 ease-in-out">
                         <i className="bi bi-pencil-fill"></i> &nbsp; Edit Profile 
 
                     <div className='ms-auto'>
@@ -54,7 +60,8 @@ const Setting = () => {
             {/* ไพรเวทซี่ชีกึม */}
             <Link to="/" className='text-decoration-none d-block'>            
                 <div className="d-flex justify-content-center mt-3">
-                    <div className="w-75 p-2 rounded bg-warning d-flex align-items-center fw-semibold text-dark hover:scale-105 transition-all duration-200 ease-in-out">
+                    <div className="w-75 p-2 rounded bg-warning d-flex align-items-center fw-semibold text-dark
+                    hover:scale-105 transition-all duration-200 ease-in-out">
                         <i className="bi bi-shield-lock-fill"></i> &nbsp; Privacy Policy
 
                     <div className='ms-auto'>
@@ -67,8 +74,6 @@ const Setting = () => {
             {/* log out */}
             <Link to="/login" className='text-decoration-none d-block'>            
                 <div className="d-flex justify-content-center mt-3">
-                    {/* duration-200 = ความเร็ว 0.2 s 
-                    ease-in-out ก็ให้ตอนเปลี่ยนมันสมูท*/}
                     <div className="w-75 p-2 rounded bg-warning d-flex align-items-center fw-semibold text-dark
                     hover:scale-105 transition-all duration-200 ease-in-out">
                         &nbsp; Log out
