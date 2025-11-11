d73b70f0a615f7f87b521b1e9b6b5f48732c8019
Pannaphatkiatniracha <compannaphat@gmail.com>
2025-11-11 22:10:18 +0700

    ลบหน้า checkinforfriendออก

--- easy-check/src/App.jsx
+++ easy-check/src/App.jsx
@@ -10,14 +10,14 @@ import Home from "./user/page/Home";
 import LeaveRequest from "./user/page/LeaveRequest";
 import CheckIn from "./user/page/CheckIn";
 import InRegister from "./user/page/InRegister";
-import DelegateCheckin from "./user/page/DelegateCheckin";
+
 import Support from "./user/page/Support";
 import ForgotPassword from "./user/page/ForgotPassword";
 import ChangePassword from "./user/page/ChangePassword";
 import InternalEvent from "./user/page/InternalEvent";
 import ExternalEvent from "./user/page/ExternalEvent";
 import ExRegister from "./user/page/ExRegister";
-import CheckInForFriend from "./user/page/CheckInForFriend";
+
 import PrivacyPolicy from "./user/page/PrivacyPolicy";
 import { useState } from "react";
 
@@ -59,13 +59,13 @@ function App() {
                 <Route path="exregister" element={<ExRegister />} />
                 <Route path="leaverequest" element={<LeaveRequest />} />
                 <Route path="checkin" element={<CheckIn />} />
-                <Route path="delegatecheckin" element={<DelegateCheckin />} />
+                
                 <Route path="support" element={<Support />} />
                 <Route path="forgotpassword" element={<ForgotPassword />} />
                 <Route path="changepassword" element={<ChangePassword />} />
                 <Route path="internalevent" element={<InternalEvent />} />
                 <Route path="externalevent" element={<ExternalEvent />} />
-                <Route path="checkinforfriend" element={<CheckInForFriend />} />
+               
                 <Route path="privacypolicy" element={<PrivacyPolicy />} />
               </Route>
             </>
