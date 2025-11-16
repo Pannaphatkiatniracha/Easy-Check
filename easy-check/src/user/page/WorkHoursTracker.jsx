13175b791794a284ca4ca72835f66c60a58a1fa8
thitichat-11 <thitichatphoto@gmail.com>
2025-11-17 04:05:24 +0700

    ทำหน้า WorkHoursTracker

--- easy-check/src/user/page/WorkHoursTracker.jsx
+++ easy-check/src/user/page/WorkHoursTracker.jsx
@@ -1,7 +1,158 @@
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
 const WorkHoursTracker = () => {
+
+    const weekTemplate = [
+        {
+            day: "Monday",
+            color: "#FFD700"
+        },
+        {
+            day: "Tuesday",
+            color: "#FF69B4"
+        },
+        {
+            day: "Wednesday",
+            color: "#32CD32"
+        },
+        {
+            day: "Thursday",
+            color: "#FFA500"
+        },
+        {
+            day: "Friday",
+            color: "#1E90FF"
+        },
+    ]
+
+
+    const hoursData = {
+        Monday: 80,
+        Tuesday: 60,
+        Wednesday: 100,
+        Thursday: 50,
+        Friday: 90,
+    }
+    
+    
+    // กำหนดชื่อวันโดยเรียงตาม date 0-6
+    const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    
+    // new Date() บอกว่าวันนี้วันที่ 17 ซึ่งตรงกับวันจันทร์ getDay() ก็จะส่งเลข 1
+    // และ dayNames ที่เรากำหนดไว้ก็คือเรียงให้มันตรงกับค่าของมัน
+
+    // ผลลัพธ์คือ todayIndex = 1 todayName = "Monday"
+    const todayIndex = new Date().getDay() // วันปัจจุบันจริง ๆ
+    const todayName = dayNames[todayIndex]
+
+
+
+    // item มันก็คือ objects ของวันนั้น ๆ
+    const weeklyData = weekTemplate.map(item => {
+
+
+        // dayNames.indexOf(item.day) จะได้ ตำแหน่งของวันนั้นในสัปดาห์ เช่นแบบ dayNames.indexOf("Wednesday") คืนค่า 3 
+        const itemIndex = dayNames.indexOf(item.day) // itemIndex = เลขลำดับวันนั้นๆ
+
+        const reached = itemIndex <= todayIndex
+        return {
+            ...item,
+            percent: reached ? (hoursData[item.day] ?? 0) : 0, // ให้เห็นภาพก็แบบ hoursData["Monday"] → 80 ?? คือเช็คว่ามีวันไหนเป็น undefind ไหม ถ้ามีจะเป็น 0 ซึ่งสมมติใน array มีค่าเป็น 0 ก็ถือว่ามีนะ ต้องไม่ใส่ค่าให้มันถึงเป็น undefind
+            color: reached ? item.color : "#ccc"
+        }
+    })
+
+
+    // .find() มันคือของอย่างที่แรกที่ระบบเจอและตรงตามเงื่อนไข
+    // today อันนี้คือพอเช็คแล้วว่า d => d.day === todayName ถูกไหม ถ้าถูกมันจะได้ชุดผลลัพธ์ทั้งหมดเลย เช่น วันจันทร์ สีเหลือง 80%
+    const today = weeklyData.find(d => d.day === todayName) ?? { percent: 0, color: "#ccc" }
+
+    // d = ตัวแทนของ แต่ละออบเจ็กต์ใน weeklyData (แบบทั้งหมด day,color,percent) ทีละตัว ซึ่ง d.day ก็คือเฉพาะ day
+    // ซึ่ง todayName = วันของปัจจุบัน
+    // d => d.day === todayName อันนี้คือเฉพาะวันแบบ Monday Tuesday Wednesday / weeklyData ถึงจะเป็นวันที่แบบตัวเลข
+
+
     return (
         <div className="app-container">
-            <h2>Work Hours Tracker</h2>
+
+            {/* หัวข้อ */}
+            <div className="d-flex justify-content-between text-white mt-16">
+
+                {/* variant เป็น link = ปุ่มไม่มีพื้นหลัง แล้วก็ลบ padding ออก */}
+                <Link to="/home" className='text-decoration-none'>
+                    <Button variant="link" className="p-0">
+                        <i className="bi bi-chevron-left ms-3 text-white"></i>
+                    </Button>
+                </Link>
+
+                <h3 className="fw-bold">Work Hours Tracker</h3>
+                {/* สร้างกล่องปลอมมาแล้วก็ใช้ margin end ช่วยให้เลเอ้ามันตรงกับดีไซน์ */}
+                <div className="me-4"></div>
+            </div>
+
+
+            {/* ขแงวันนี้ */}
+            <div className='d-flex justify-content-center mt-10'>
+                <div className="p-2 px-1 text-center fw-semibold rounded-3 text-dark w-80"
+                    style={{ background: 'linear-gradient(to bottom, #D9D9D9, #636CCB)' }}>
+
+                    <h4 className="mt-3 mb-3 fw-bold">Today</h4>
+                    <div className='p-1'>
+
+                        <ProgressBar className='mb-3' 
+                        style={{ height: '20px', backgroundColor: '#ccc'}}>
+
+                            <ProgressBar className='text-black' now={today.percent} key={1} label={`${today.percent}%`}
+                            style={{ backgroundColor: today.color }}/>
+                            
+                        </ProgressBar>
+
+                    </div>
+
+                </div>
+            </div>
+
+
+
+            {/* รายสัปดาห์ */}
+            <div className='d-flex justify-content-center mt-8'>
+                <div className="p-2 px-1 text-center fw-semibold rounded-3 text-dark w-80"
+                    style={{ background: 'linear-gradient(to bottom, #D9D9D9, #636CCB)' }}>
+
+                    <h4 className="mt-3 mb-6 fw-bold">Weekly</h4>
+
+
+                    {weeklyData.map((item, index) => (
+                        <div key={index} className='mb-3 ml-1 mr-1 text-start'>
+
+                            {/* ชื่อกับเปอร์เซนต์ที่แสดง */}
+                            <div className='d-flex justify-content-between mb-1'>
+                                <span>{item.day}</span>
+                                <span>{item.percent}%</span>
+                            </div>
+
+                            {/* ตรง now ไว้กำหนดความยาวของแถบสี แต่มันเลือกสีตามที่เราต้องการไม่ได้เลยต้องกำหนดใน div ข้างในอีกที */}
+                            <ProgressBar now={item.percent} className="mb-2"
+                                style={{ backgroundColor: '#ccc', height: '20px' }}>
+                                <div style={{
+                                    width: `${item.percent}%`,
+                                    backgroundColor: item.color,
+                                    }}/>
+                            </ProgressBar>
+
+                        </div>
+                    ))}
+
+                </div>
+            </div>
+
+
+
+
+
+
         </div>
     )
 }
