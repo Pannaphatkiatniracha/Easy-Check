9e595bbf2dd3820a51fcd5ce486432a7b80f4a13
Pannaphatkiatniracha <compannaphat@gmail.com>
2025-11-24 18:50:19 +0700

    แก้ชื่อ

--- easy-check/src/user/page/Home.jsx
+++ easy-check/src/user/page/Home.jsx
@@ -39,7 +39,7 @@ const Home = ({ role }) => {
                     hover:scale-105 transition-all duration-200 ease-in-out"
                         style={{ background: 'linear-gradient(to bottom, #D9D9D9, #636CCB)' }}>
                         <i className="bi bi-calendar-check-fill fs-2"></i> <br />
-                        DELEGATE APPROVE
+                        APPROVER MANAGEMENT
                     </div>
                 </Link>
 
