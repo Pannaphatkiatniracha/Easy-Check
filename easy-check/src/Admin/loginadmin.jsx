a1b96639dbb382036ea5d5ca484b1b526d2dea11
sarasinee67130909 <sarasinee.boo@spumail.net>
2025-11-26 02:24:54 +0700

    แก้ชื่อพิมพ์ใหญ่

--- easy-check/src/Admin/loginadmin.jsx
+++ easy-check/src/Admin/loginadmin.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { authenticateUser } from '../../data/mockUsers';
-import './loginadmin.css';
+import './Loginadmin.css';
 
 const Login = () => {
   const [formData, setFormData] = useState({
