a8c0c34e420eea89f712fe3cc01645715036c7ce
Thanik98 <thanik1265@gmail.com>
2025-11-26 18:55:37 +0700

    import ในส่วน Admin

--- easy-check/src/App.jsx
+++ easy-check/src/App.jsx
@@ -29,7 +29,11 @@ import DataToCheck from "./user/page/DataToCheck";
 import ApproverManagement from "./user/page/ApproverManagement";
 import PaymentRequest from "./user/page/PaymentRequest"; // KEEP BOTH
 import Notification from "./user/page/Notification";
-
+import Dashboard from "./Admin/Dashboard.jsx";
+import SetGPS from "./Admin/SetGPS.jsx";
+import Personalsummary from "./Admin/Personalsummary.jsx";
+import MyProfile from "./Admin/MyProfile.jsx";
+import Manageusers from "./Admin/Manageusers.jsx";
 
 function App() {
   const [token, setToken] = useState(() => localStorage.getItem("token") || "");
