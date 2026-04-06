d3d87068c850a6802c1f4f702d888aa07618523a
Pannaphatkiatniracha <compannaphat@gmail.com>
2026-04-06 23:50:21 +0700

    ได้แล้ว

--- easycheck-backend/src/routers/attendanceRouter.js
+++ easycheck-backend/src/routers/attendanceRouter.js
@@ -1,6 +1,5 @@
 import express from "express";
 import {
-
   checkIn,
   checkOut,
   getHistory,
@@ -9,30 +8,20 @@ import {
   rejectAttendance,
   getMyShift,
   upload,
-
-  checkIn, checkOut, getHistory,
-  approveAttendance, rejectAttendance, upload,
-  getAttendanceHistory, getWeeklyHours, getCurrentStatus
-
 } from "../controllers/attendanceController.js";
 import { verifyToken } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
+// User
 router.get("/shift/me", verifyToken, getMyShift);
 router.post("/check-in", verifyToken, upload.single("photo"), checkIn);
 router.post("/check-out", verifyToken, upload.single("photo"), checkOut);
 router.get("/history", verifyToken, getHistory);
 
+// Approver / Admin
 router.get("/pending", verifyToken, getPending);
 router.put("/:id/approve", verifyToken, approveAttendance);
 router.put("/:id/reject", verifyToken, rejectAttendance);
-
-// -----------------------------------------------------------
-router.get("/attendance-history", getAttendanceHistory)
-
-router.get('/weekly-hours', getWeeklyHours)
-
-router.get('/current-status', getCurrentStatus)
 
 export default router;
